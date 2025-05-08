7514b2407bb3e34beda9cb5d5804cfa3f606b435
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-05-08 18:00:46 +0330

    feat(main, shop): add site settings model, access control middleware, and offer system integration
    - Created `Settings` model with various fields to control site-wide configurations, including `site_access`.
    - Developed `SiteAccessMiddleware` to restrict site access based on the `site_access` field in settings.
    - Set up admin page for managing site settings with limitation to a single settings object, redirecting admin accordingly.
    - Added `verbose_name` to the main app for better identification in admin.
    - Created custom `404.html` page to handle unauthorized access attempts and when `site_access=False`.
    - Updated `index.html` to integrate with the new offer system defined in the settings model.
    - Fixed translation issue in the `taste` field of the shop app’s Product model.
    - Enhanced shop app’s product list view to support filtering based on the newly added offer system.

--- Neyanbar/settings.py
+++ Neyanbar/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middlewares.SiteAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'Neyanbar.urls'
--- main/models.py
+++ main/models.py
@@ -1,3 +1,56 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from shop.models import Product, Brand
+
+
+class Setting(models.Model):
+    objects = models.Manager()
+
+    site_access = models.BooleanField(default=True, verbose_name='دسترسی به سایت', help_text='دسترسی همه کاربران از سایت قطع می شود.')
+
+    post_fee = models.PositiveIntegerField(default=10, validators=[MinValueValidator(10)], verbose_name='کرایه پست', help_text='به تومان')
+    tax_fee = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)], verbose_name='مالیات', help_text='به درصد')
+
+    show_offer = models.BooleanField(default=False, verbose_name='نمایش پیشنهاد')
+    title = models.CharField(max_length=30, blank=True, verbose_name='تیتر پیشنهاد')
+    event = models.CharField(max_length=100, blank=True, verbose_name='(مناسبت/علت تخفیف) پیشنهاد')
+    products = models.CharField(max_length=50, blank=True, verbose_name='محصولات انتخاب شده', help_text='بر اساس لیست پیشنهاد های مجاز')
+    banner = models.ImageField(upload_to='offers/', blank=True, verbose_name='بنر')
+
+    def get_offer_link(self):
+        if not self.show_offer or not self.products:
+            return ''
+        args = self.products.split(':')[1]
+        if self.products.startswith('products:'):
+            if not ';' in args:
+                return reverse('shop:product-detail', kwargs={'slug': args})
+            return reverse('shop:product-list') + f'?products={args}'
+        elif self.products.startswith('brands:'):
+            return reverse('shop:product-list') + f'?brands={args}'
+        elif self.products.startswith('volumes:'):
+            return reverse('shop:product-list') + f'?volumes={args}'
+        elif self.products.startswith('smells'):
+            return reverse('shop:product-list') + f'?smells={args}'
+        elif self.products.startswith('seasons:'):
+            return reverse('shop:product-list') + f'?seasons={args}'
+        elif self.products.startswith('tastes:'):
+            return reverse('shop:product-list') + f'?tastes={args}'
+        elif self.products.startswith('nature'):
+            return reverse('shop:product-list') + f'?nature={args}'
+        elif self.products.startswith('durability:'):
+            return reverse('shop:product-list') + f'?durability={args}'
+        elif self.products.startswith('gender'):
+            return reverse('shop:product-list') + f'?gender={args}'
+        elif self.products.startswith('type:'):
+            return reverse('shop:product-list') + f'?type={args}'
+        return ''
+
+    def __str__(self):
+        return 'تنظیمات'
+
+    class Meta:
+        verbose_name = verbose_name_plural = 'تنظیمات'
+
+
