cc34930696643cb8eba8c79b2cab47ad16f2e450
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-05-28 16:02:22 +0330

    refactor(notifications): enhance discount and order notifications with fixes and automation
    - Improved discount notifications.
    - Updated order submitted notification to inform users they can track their orders from the dashboard.
    - Added order submitted notification for admins to keep them informed.
    - Fixed issue where "notify me" product feature did not remove users from the notification list after action.
    - Automated the "notify me" product notification process for better user experience.
    - Conducted comprehensive testing of all notifications to ensure reliability.

--- main/utilities.py
+++ main/utilities.py
@@ -6,16 +6,18 @@ env = settings.ENV
 LOGIN_VERIFY = env('LOGIN_VERIFY')
 PRODUCT_NOTIFY_ME = env('PRODUCT_NOTIFY_ME')
 ORDER_SUBMITTED = env('ORDER_SUBMITTED')
+ORDER_SUBMITTED_ADMIN = env('ORDER_SUBMITTED_ADMIN')
 ORDER_PAID = env('ORDER_PAID')
 ORDER_CONFIRMED = env('ORDER_CONFIRMED')
 ORDER_IN_PROGRESS = env('ORDER_IN_PROGRESS')
 ORDER_FINISHED = env('ORDER_FINISHED')
 ORDER_SHIPPED = env('ORDER_SHIPPED')
 ORDER_REJECTED = env('ORDER_REJECTED')
+ADMIN_PHONE = env('ADMIN_PHONE')
 
 
 def send_sms(phone, bodyID, *args):
     if bodyID is None:
-        response = sms.send(phone, '50004001766438', args[0])
+        return sms.send(phone, '50004001766438', args[0])
     else:
-        sms.send_by_base_number(';'.join(str(arg) for arg in args), phone, bodyID)
+        return sms.send_by_base_number(';'.join(str(arg) for arg in args), phone, bodyID)
